aa983062d5d79a06135c1b1688ce0b5b0e2b4476
Delph1 <andreas.galistel@gmail.com>
2025-04-12 15:06:06 +0200

    Orderconfirmation header almost done

--- Hattmakarna/src/hattmakarna/Print.java
+++ Hattmakarna/src/hattmakarna/Print.java
@@ -15,59 +15,73 @@ import org.apache.pdfbox.pdmodel.font.PDType1Font;
  * @author nolle
  */
 public class Print {
-private Order order;
-PDDocument document = new PDDocument();
-
+    
+    PDDocument document = new PDDocument();
+    private Order order;
+    private Customer customer;
+    private OrderLine orderline;
+    
     public Print (Order order) throws IOException{
- 
-    int orderId = order.getId();
+
+        //Base data collection
+        String headerText = "Orderbekräftelse";
+        int orderId = order.getId();
+        int customerId = order.getCustomer_id();
+        customer = dbm.getCustomer(customerId);
+        String state = customer.getState();
+        
+        
+        //Setting up the document for PDFbox
+        PDPage page = new PDPage();
+        document.addPage(page);
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        contentStream.beginText();
+        
+        //Document header, y offset is from the bottom of the document = higher number, higher up on the page, x offset is the from the left border. Also, offset is RELATIVE, NOT ABSOLUTE.
+        contentStream.setFont(PDType1Font.HELVETICA, 24);
+        contentStream.newLineAtOffset(50, 740);
+        contentStream.showText(headerText);
+        contentStream.endText();
+        contentStream.beginText();
+        contentStream.setFont(PDType1Font.HELVETICA, 14);
+        contentStream.newLineAtOffset(450, 700);
+        contentStream.showText("Ordernummer: " + Integer.toString(orderId));
+
+        //Customer data output below
+        contentStream.newLineAtOffset(-400, 0);
+        contentStream.showText("Kundnummer: " + Integer.toString(customerId));
+        contentStream.newLineAtOffset(0, -16);
+        contentStream.showText("Namn: " + customer.getFirstName() + " " + customer.getLastName());
+        contentStream.newLineAtOffset(0, -16);
+        contentStream.showText("Gata: " + customer.getStreetName());
+        contentStream.newLineAtOffset(0, -16);
+        contentStream.showText("Postadress: " + customer.getPostalCode() + " " + customer.getPostalCity());
+        if ( state != null && !state.isEmpty()) {
+            contentStream.newLineAtOffset(0, -16);
+            contentStream.showText("Delstat: " + state);
+        }
+        contentStream.newLineAtOffset(0, -16);
+        contentStream.showText("Land: " + customer.getCountry());
+        
+        contentStream.endText();
+
+        contentStream.close(); 
+
     
-    PDPage newPage = new PDPage();
-    
-    document.addPage(newPage);
-    
-    PDPageContentStream contentStream = new PDPageContentStream(document, newPage);
-    
-    contentStream.beginText();
-    
-    contentStream.setFont(PDType1Font.HELVETICA, 14);
-    
-    contentStream.newLineAtOffset(50, 50);
-    
-    contentStream.showText(Integer.toString(orderId));
-    
-    contentStream.endText();
-    
-    contentStream.close(); 
-    
-    
-    
-    
-    
-    
-    
-    String path = "C:\\Users\\nolle\\Downloads\\test.pdf";
-   
-    try {
-    document.save(path);     
-    }
-    
-    catch(Exception e) {
-    System.out.println("");
-    }
-    
-    try {
-    document.close();
-    }
-    
-    catch (Exception e) {
-    System.out.println("");
+        //Saves the file to the project folder
+        String path = "./test.pdf";
+        try {
+            document.save(path);  
+        } catch(Exception e) {
+            System.err.println("");
+        }
+        try {
+            document.close();
+        }
+        catch (Exception e) {
+            System.err.println("");
     }
   }
-
-public static void main (String [] args) {
-       
-}
 }
 
 
