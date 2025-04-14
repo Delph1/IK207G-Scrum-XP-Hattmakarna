572be0a317a1f9dec3667d3810fd7f1ff7ff41cb
PetraKSchroder <petra.s.kampe@gmail.com>
2025-04-14 11:45:32 +0200

    Schema (#22)
    * Class Customer
    
    * Class Customer
    
    * class ProductComponent
    
    * class Product
    
    * Class shipment
    
    * class User
    
    * class Image
    
    * class OrderLine
    
    * orderList
    
    * List
    
    * Orderpanel
    
    * Orderpanel - check
    
    * Schema
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Fel
    
    * Update built-jar.properties
    
    Signed-off-by: David Törnros <126451299+3xyl3x@users.noreply.github.com>
    
    * Update project.properties
    
    removed pdfbox 3 reference
    
    Signed-off-by: Delph1 <andreas.galistel@gmail.com>
    
    ---------
    
    Signed-off-by: David Törnros <126451299+3xyl3x@users.noreply.github.com>
    Signed-off-by: Delph1 <andreas.galistel@gmail.com>
    Co-authored-by: David Törnros <126451299+3xyl3x@users.noreply.github.com>
    Co-authored-by: Delph1 <andreas.galistel@gmail.com>

--- Hattmakarna/src/panels/OrderPanel.java
+++ Hattmakarna/src/panels/OrderPanel.java
@@ -5,6 +5,7 @@ import hattmakarna.MainWindow;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.JOptionPane;
 //import models.*;
+
 import models.Order;
 
 
@@ -13,6 +14,14 @@ public class OrderPanel extends javax.swing.JPanel {
     private Order order;
     
     public OrderPanel(MainWindow window, int orderId) {
+
+        // Vi tar emot och lagrar huvudfönstret som ett fält, då kan vi komma åt metoder som att byta panel
+        this.window=window; 
+        initComponents();
+        Order order = dbm.getOrder(orderId);
+    }
+    
+    public OrderPanel(MainWindow window) {
         // Vi tar emot och lagrar huvudfönstret som ett fält, då kan vi komma åt metoder som att byta panel
         this.window=window; 
         initComponents();
