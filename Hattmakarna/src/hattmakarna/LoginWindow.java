676c9df1bc62264e073066a87e2669a9eec2a4ac
Delph1 <andreas.galistel@gmail.com>
2025-04-29 17:04:56 +0200

    Sista fixar (#146)
    * Nya databasemetoder för ProductImage
    Knapp i MainWindow
    
    * Fixar för felaktig konfliktfix
    
    * Bugfixar
    
    * Tagit bort Otto och Edith-knapparna
    
    * Ändrat Schema-rubriken.
    
    * Snyggat till koden lite

--- Hattmakarna/src/hattmakarna/LoginWindow.java
+++ Hattmakarna/src/hattmakarna/LoginWindow.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package hattmakarna;
 
 import javax.swing.JOptionPane;
@@ -12,34 +9,30 @@ public class LoginWindow extends javax.swing.JFrame {
     
     public LoginWindow() {
         initComponents();
-        
-
-    //SwingUtilities.invokeLater(() -> loginBTN.doClick());// Simulerar loginklick
     }
     
     public void loggaIn(){
        String username = userNameText.getText().trim();
        String losenord = new String(losenText.getPassword()).trim();
        
-  if (username.isEmpty() || losenord.isEmpty()) {
+        if (username.isEmpty() || losenord.isEmpty()) {
             JOptionPane.showMessageDialog(this, "Du måste fylla i både användarnamn och lösenord");
             return;
         }
 
-   User user = dbm.login(username, losenord);
+        User user = dbm.login(username, losenord);
 
-    if (user != null) {
-//        JOptionPane.showMessageDialog(this, "Inloggning lyckades. Välkommen " + user.getUserName());
-        Hattmakarna.currentUser = user;
-        MainWindow window = new MainWindow();
-        window.setVisible(true);
-        window.setExtendedState(window.getExtendedState()|window.MAXIMIZED_BOTH);
-        this.dispose();
-        
-    } else {
-        JOptionPane.showMessageDialog(this, "Felaktigt användarnamn eller lösenord.");
+        if (user != null) {
+            Hattmakarna.currentUser = user;
+            MainWindow window = new MainWindow();
+            window.setVisible(true);
+            window.setExtendedState(window.getExtendedState()|window.MAXIMIZED_BOTH);
+            this.dispose();
+
+        } else {
+            JOptionPane.showMessageDialog(this, "Felaktigt användarnamn eller lösenord.");
+        }
     }
-}
  
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -51,8 +44,6 @@ public class LoginWindow extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         userNameText = new javax.swing.JTextField();
         losenText = new javax.swing.JPasswordField();
-        ottoLogin = new javax.swing.JButton();
-        edithLogin = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -73,20 +64,6 @@ public class LoginWindow extends javax.swing.JFrame {
         losenText.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 losenTextActionPerformed(evt);
-            }
-        });
-
-        ottoLogin.setText("OTTO");
-        ottoLogin.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ottoLoginActionPerformed(evt);
-            }
-        });
-
-        edithLogin.setText("EDITH");
-        edithLogin.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                edithLoginActionPerformed(evt);
             }
         });
 
@@ -111,15 +88,8 @@ public class LoginWindow extends javax.swing.JFrame {
                 .addContainerGap(124, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(loginBTN, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(132, 132, 132))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(edithLogin)
-                            .addComponent(ottoLogin))
-                        .addGap(40, 40, 40))))
+                .addComponent(loginBTN, javax.swing.GroupLayout.PREFERRED_SIZE, 210, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(132, 132, 132))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -136,11 +106,7 @@ public class LoginWindow extends javax.swing.JFrame {
                     .addComponent(losenText, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 44, Short.MAX_VALUE)
                 .addComponent(loginBTN)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(ottoLogin)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(edithLogin)
-                .addGap(26, 26, 26))
+                .addGap(90, 90, 90))
         );
 
         pack();
@@ -154,24 +120,12 @@ public class LoginWindow extends javax.swing.JFrame {
         loggaIn();
     }//GEN-LAST:event_losenTextActionPerformed
 
-    private void ottoLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ottoLoginActionPerformed
-        userNameText.setText("Otto");
-        losenText.setText("admin1");
-    }//GEN-LAST:event_ottoLoginActionPerformed
-
-    private void edithLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_edithLoginActionPerformed
-        userNameText.setText("edith");
-        losenText.setText("admin2");
-    }//GEN-LAST:event_edithLoginActionPerformed
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton edithLogin;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JButton loginBTN;
     private javax.swing.JPasswordField losenText;
-    private javax.swing.JButton ottoLogin;
     private javax.swing.JTextField userNameText;
     // End of variables declaration//GEN-END:variables
 }
