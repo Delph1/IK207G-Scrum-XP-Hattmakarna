ac4e659c81172ace7dc43f1b320260d727871905
Delph1 <andreas.galistel@gmail.com>
2025-04-29 12:33:47 +0200

    CreateOrder: open -> Open

--- Hattmakarna/src/database/DatabaseManager.java
+++ Hattmakarna/src/database/DatabaseManager.java
@@ -88,7 +88,7 @@ public class DatabaseManager {
             int newId = (maxIdStr == null || maxIdStr.isEmpty()) ? 1 : Integer.parseInt(maxIdStr) + 1;
             // Lägger in beställning i databasen
 
-            db.insert("INSERT INTO orders (order_id,customer_id,order_date,order_status, express, shipping_cost) values (" + newId + ",1,NOW(),'open', 0, 39)");
+            db.insert("INSERT INTO orders (order_id,customer_id,order_date,order_status, express, shipping_cost) values (" + newId + ",1,NOW(),'Open', 0, 39)");
 
             // Returnerar den nya beställningen
             return getOrder(newId);
